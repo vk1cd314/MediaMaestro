12c4d904aa8f09b09b6c6d524ecf590a509b25ff
sinhSlumbering <muztoba.sinha@gmail.com>
2023-04-28 02:47:03 +0600

    added search

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
 
-"""Simple HTTP Server With Upload.
-This module builds on BaseHTTPServer by implementing the standard GET
-and HEAD requests in a fairly straightforward manner.
-"""
 
 __version__ = "0.3.1"
-__author__ = "freelamb@126.com"
 __all__ = ["SimpleHTTPRequestHandler"]
 
 import os
@@ -23,21 +16,12 @@ import mimetypes
 import re
 import signal
 from io import StringIO, BytesIO
+import urllib.parse
 
-if sys.version_info.major == 3:
-    # Python3
-    from urllib.parse import quote
-    from urllib.parse import unquote
-    from http.server import HTTPServer
-    from http.server import BaseHTTPRequestHandler
-else:
-    # Python2
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
-    from urllib import quote
-    from urllib import unquote
-    from BaseHTTPServer import HTTPServer
-    from BaseHTTPServer import BaseHTTPRequestHandler
+from urllib.parse import quote
+from urllib.parse import unquote
+from http.server import HTTPServer
+from http.server import BaseHTTPRequestHandler
 
 
 import datetime
@@ -56,34 +40,148 @@ def format_date(timestamp):
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    """Simple HTTP request handler with GET/HEAD/POST commands.
-    This serves files from the current directory and any of its
-    subdirectories.  The MIME type for files is determined by
-    calling the .guess_type() method. And can receive file uploaded
-    by client.
-    The GET/HEAD/POST requests are identical except that the HEAD
-    request omits the actual contents of the file.
-    """
+    
 
     server_version = "simple_http_server/" + __version__
 
     def do_GET(self):
-        """Serve a GET request."""
-        fd = self.send_head()
-        if fd:
-            shutil.copyfileobj(fd, self.wfile)
-            fd.close()
+        if self.path.startswith('/search?q='):
+            # This is a search query
+            query = self.path.split('=')[1]
+            print(f"Search query: {query}")
+            path = translate_path(self.path)
+            fd = self.filter(path)
+            if fd:
+                shutil.copyfileobj(fd, self.wfile)
+                fd.close()
+            
+        else:    
+            """Serve a GET request."""
+            fd = self.send_head()
+            if fd:
+                shutil.copyfileobj(fd, self.wfile)
+                fd.close()
+
+    def filter(self, path):
+        print(path)
+        query_params = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
+        search_query = query_params.get("q", [""])[0] # default to empty string if "q" parameter is not present
+
+        # Filter files by search query
+        
+        try:
+            path = path[:-7]
+            print(path)
+            list_dir = os.listdir(path)
+            list_dir = [f for f in list_dir if search_query.lower() in f.lower()]
+        except os.error:
+            self.send_error(404, "No permission to list directory")
+            return None
+        # list_dir.sort(key=lambda a: a.lower())
+        f = BytesIO()
+        display_path = escape(unquote(self.path))
+        f.write(b'<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
+        f.write(b'<html>\n<head>\n')
+        f.write(b'<title>Directory listing for %s</title>\n' % display_path.encode('utf-8'))
+        f.write(b'<style>\n')
+        f.write(b'body { font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #F2F2F2; }\n')
+        f.write(b'.container { max-width: 800px; margin: 0 auto; padding: 20px; background-color: #FFF; box-shadow: 0 0 10px rgba(0, 0, 0, 0.3); }\n')
+        f.write(b'h1 { text-align: center; margin-bottom: 20px; }\n')
+        f.write(b'form { display: flex; flex-direction: column; align-items: center; margin-bottom: 20px; }\n')
+        f.write(b'input[type="file"] { margin-bottom: 10px; }\n')
+        f.write(b'input[type="submit"] { background-color: #4CAF50; color: #FFF; padding: 10px; border: none; border-radius: 5px; cursor: pointer; transition: background-color 0.2s ease-in-out; }\n')
+        f.write(b'input[type="submit"]:hover { background-color: #3E8E41; }\n')
+        f.write(b'table { border-collapse: collapse; width: 100%; margin-bottom: 20px; }\n')
+        f.write(b'th, td { padding: 10px; text-align: left; border-bottom: 1px solid #ddd; }\n')
+        f.write(b'th { background-color: #4CAF50; color: #FFF; }\n')
+        f.write(b'a { color: #000; text-decoration: none; }\n')
+        f.write(b'a:hover { text-decoration: underline; }\n')
+        f.write(b'</style>\n')
+        f.write(b'</head>\n')
+        f.write(b"<hr>\n")
+        f.write(b"<h1>Upload File</h1>\n")
+        f.write(b"<form ENCTYPE=\"multipart/form-data\" method=\"post\" style=\"margin-bottom: 1em;\">\n")
+        f.write(b"<input name=\"file\" type=\"file\" style=\"margin-right: 0.5em;\" />\n")
+        f.write(b"<input type=\"submit\" value=\"Upload File\" class=\"btn btn-primary\" />\n")
+        f.write(b"</form>\n")
+        f.write(b"<form action=\"/search\" method=\"get\">\n")
+        f.write(b"<label for=\"search\">Search:</label>\n")
+        f.write(b"<input type=\"search\" id=\"search\" name=\"q\" placeholder=\"Search...\">\n")
+        f.write(b"<button type=\"submit\">Go</button>\n")
+        f.write(b"</form>\n")
+        # f.write(b"<html>\n<title>Directory listing for %s</title>\n" % display_path.encode('utf-8'))
+        # f.write(b"<body>\n<h2>Directory listing for %s</h2>\n" % display_path.encode('utf-8'))
+        # f.write(b"<hr>\n")
+        # f.write(b"<form ENCTYPE=\"multipart/form-data\" method=\"post\">")
+        # f.write(b"<input name=\"file\" type=\"file\"/>")
+        # f.write(b"<input type=\"submit\" value=\"upload\"/></form>\n")
+        f.write(b"<hr>\n<ul>\n")
+        new_list = []
+        new_list_1 = []
+        # f.write(b"<h1>Directories</h1>\n")
+        for name in list_dir:
+            fullname = os.path.join(path, name)
+            display_name = linkname = name
+            # Append / for directories or @ for symbolic links
+            wat = False
+            if os.path.isdir(fullname):
+                display_name = name + "/"
+                linkname = name + "/"
+                wat = True
+            if os.path.islink(fullname):
+                display_name = name + "@"
+                wat = True
+                # Note: a link to a directory displays with @ and links with /
+            if wat:
+                # f.write(b'<li><a href="%s">%s</a>\n' % (quote(linkname).encode('utf-8'), escape(display_name).encode('utf-8')))
+                new_list_1.append((linkname, display_name))
+            else:
+                new_list.append((linkname, display_name))
+        # f.write(b"<h1>Files</h1>\n")
+        # for linkname, display_name in new_list:
+        #     f.write(b'<li><a href="%s">%s</a>\n' % (quote(linkname).encode('utf-8'), escape(display_name).encode('utf-8')))
+        f.write(b'<hr>\n<h2>Directories:</h2>\n')
+        f.write(b'<table style="width:100%">\n')
+        f.write(b'<tr>\n<th>Name</th>\n<th>Size</th>\n<th>Last Modified</th>\n</tr>\n')
+        for linkname, display_name in new_list_1:
+            f.write(b'<tr>\n')
+            f.write(b'<td><a href="%s">%s</a></td>\n' % (quote(linkname).encode('utf-8'), escape(display_name).encode('utf-8')))
+            # f.write(b'<td>%s</td>\n' % format_size(file_size).encode('utf-8'))
+            # f.write(b'<td>%s</td>\n' % format_date(mod_time).encode('utf-8'))
+            f.write(b'</tr>\n')
+        f.write(b'</table>\n')
+        f.write(b'<hr>\n<h2>Files:</h2>\n')
+        f.write(b'<table style="width:100%">\n')
+        f.write(b'<tr>\n<th>Name</th>\n<th>Size</th>\n<th>Last Modified</th>\n</tr>\n')
+        for linkname, display_name in new_list:
+            f.write(b'<tr>\n')
+            f.write(b'<td><a href="%s">%s</a></td>\n' % (quote(linkname).encode('utf-8'), escape(display_name).encode('utf-8')))
+            # f.write(b'<td>%s</td>\n' % format_size(file_size).encode('utf-8'))
+            # f.write(b'<td>%s</td>\n' % format_date(mod_time).encode('utf-8'))
+            f.write(b'</tr>\n')
+        f.write(b'</table>\n')
+
+        f.write(b"</ul>\n<hr>\n</body>\n</html>\n")
+        length = f.tell()
+        f.seek(0)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html;charset=utf-8")
+        self.send_header("Content-Length", str(length))
+        self.end_headers()
+        return f
+
+        
 
     def do_HEAD(self):
-        """Serve a HEAD request."""
+
         fd = self.send_head()
         if fd:
             fd.close()
 
     def do_POST(self):
-        """Serve a POST request."""
+
         r, info = self.deal_post_data()
-        print(r, info, "by: ", self.client_address)
+        print(r, info, "byNolaXoda: ", self.client_address)
         f = BytesIO()
         f.write(b'<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
         f.write(b"<html>\n<title>Upload Result Page</title>\n")
@@ -95,8 +193,8 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             f.write(b"<strong>Failed:</strong>")
         f.write(info.encode('utf-8'))
         f.write(b"<br><a href=\".\">back</a>")
-        f.write(b"<hr><small>Powered By: freelamb, check new version at ")
-        f.write(b"<a href=\"https://github.com/freelamb/simple_http_server\">")
+        f.write(b"<hr><small>Powered By: Tsunderead - 1")
+        f.write(b"<a href=\"https://www.youtube.com/watch?v=dQw4w9WgXcQ\">")
         f.write(b"here</a>.</small></body>\n</html>\n")
         length = f.tell()
         f.seek(0)
@@ -109,6 +207,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             f.close()
 
     def deal_post_data(self):
+        print(self.headers)
         boundary = self.headers["Content-Type"].split("=")[1].encode('utf-8')
         remain_bytes = int(self.headers['content-length'])
         line = self.rfile.readline()
@@ -151,13 +250,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         return False, "Unexpect Ends of data."
 
     def send_head(self):
-        """Common code for GET and HEAD commands.
-        This sends the response code and MIME headers.
-        Return value is either a file object (which has to be copied
-        to the output file by the caller unless the command was HEAD,
-        and must be closed by the caller under all circumstances), or
-        None, in which case the caller has nothing further to do.
-        """
+        
         path = translate_path(self.path)
         if os.path.isdir(path):
             if not self.path.endswith('/'):
@@ -191,11 +284,8 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         return f
 
     def list_directory(self, path):
-        """Helper to produce a directory listing (absent index.html).
-        Return value is either a file object, or None (indicating an
-        error).  In either case, the headers are sent, making the
-        interface the same as for send_head().
-        """
+        print(path)
+       
         try:
             list_dir = os.listdir(path)
         except os.error:
@@ -227,6 +317,11 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         f.write(b"<form ENCTYPE=\"multipart/form-data\" method=\"post\" style=\"margin-bottom: 1em;\">\n")
         f.write(b"<input name=\"file\" type=\"file\" style=\"margin-right: 0.5em;\" />\n")
         f.write(b"<input type=\"submit\" value=\"Upload File\" class=\"btn btn-primary\" />\n")
+        f.write(b"</form>\n")
+        f.write(b"<form action=\"/search\" method=\"get\">\n")
+        f.write(b"<label for=\"search\">Search:</label>\n")
+        f.write(b"<input type=\"search\" id=\"search\" name=\"q\" placeholder=\"Search...\">\n")
+        f.write(b"<button type=\"submit\">Go</button>\n")
         f.write(b"</form>\n")
         # f.write(b"<html>\n<title>Directory listing for %s</title>\n" % display_path.encode('utf-8'))
         # f.write(b"<body>\n<h2>Directory listing for %s</h2>\n" % display_path.encode('utf-8'))
@@ -290,15 +385,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         return f
 
     def guess_type(self, path):
-        """Guess the type of a file.
-        Argument is a PATH (a filename).
-        Return value is a string of the form type/subtype,
-        usable for a MIME Content-type header.
-        The default implementation looks the file's extension
-        up in the table self.extensions_map, using application/octet-stream
-        as a default; however it would be permissible (if
-        slow) to look inside the data to make a better guess.
-        """
+    
 
         base, ext = posixpath.splitext(path)
         if ext in self.extensions_map:
@@ -321,11 +408,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
 
 def translate_path(path):
-    """Translate a /-separated PATH to the local filename syntax.
-    Components that mean special things to the local file system
-    (e.g. drive or directory names) are ignored.  (XXX They should
-    probably be diagnosed.)
-    """
     # abandon query parameters
     path = path.split('?', 1)[0]
     path = path.split('#', 1)[0]
@@ -343,7 +425,6 @@ def translate_path(path):
 
 
 def signal_handler(signal, frame):
-    print("You choose to stop me.")
     exit()
 
 def _argparse():
